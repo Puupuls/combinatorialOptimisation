e548c7e8762ac84abd74f51a94918eedff30c991
Paul <paulspurvins@gmail.com>
2023-09-27 23:49:49 +0300

    Add readme.md

--- optimizer.py
+++ optimizer.py
@@ -1,7 +1,3 @@
-# Ir doti n punkti, kur katram ir noteikta vērtība v[i].
-# Ir dots sākuma punkts s un finiša punkts f (vai tas ir tas pats?).
-# Dota laika matrica d, kurā jau iepriekš sarēķināts nepieciešamais laiks, lai nokļūtu no katra uz katru punktu.
-# Uzdevums ir atrast tādu punktu virkni, kas sākas punktā s un beidzas punktā f un dod vislielāko vērtību, bet prasa ne vairāk laika kā dotais limits t.
 from copy import deepcopy
 import numpy as np
 from my_types import Point, Domain, Solution, GraphNode
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Solution for Latvian University Combinatorial Optimisation course
+
+## Task:
+```
+Ir doti n punkti, kur katram ir noteikta vērtība v[i].
+Ir dots sākuma punkts s un finiša punkts f (vai tas ir tas pats?).
+Dota laika matrica d, kurā jau iepriekš sarēķināts nepieciešamais laiks, lai nokļūtu no katra uz katru punktu.
+Uzdevums ir atrast tādu punktu virkni, kas sākas punktā s un beidzas punktā f un dod vislielāko vērtību, bet prasa ne vairāk laika kā dotais limits t.
+```
